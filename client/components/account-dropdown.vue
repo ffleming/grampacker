ddb0721eb1c12bd3d42ec7120aa3c2dd949dcbf2
Forrest Fleming <ffleming@gmail.com>
2024-05-22 10:29:40 -0700

    recreate proper UI like lighterpack's

--- client/components/account-dropdown.vue
+++ client/components/account-dropdown.vue
@@ -3,10 +3,10 @@
 
 <template>
     <span class="headerItem hasPopover">
-        <Popper id="headerPopover">
+        <Popper hover>
             <span class="lpTarget">Logged in as <span class="username">{{ username }}</span> <i class="lpSprite lpExpand" /></span>
             <template #content>
-              <div class="lpContent">
+              <div class="lpPopoverContent">
                 <a class="lpHref accountSettings" @click="showAccount">Account Settings</a><br>
                 <a class="lpHref" @click="showHelp">Help</a><br>
                 <a class="lpHref signout" @click="signout">Sign Out</a>
