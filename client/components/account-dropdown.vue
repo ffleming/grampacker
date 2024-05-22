73524228578c201599db9fe0a52a3dcd92f2b8ef
Forrest Fleming <ffleming@gmail.com>
2024-05-21 22:30:49 -0700

    use vue-popper

--- client/components/account-dropdown.vue
+++ client/components/account-dropdown.vue
@@ -1,32 +1,158 @@
 <style lang="scss">
-#headerPopover .lpContent {
-    min-width: 9em;
+@import "../css/_globals";
+#csvUrl {
+    display: block;
+    margin-top: 15px;
 }
-</style>
 
+#lpOptionalFields {
+    margin: 0;
+    padding: 0;
+}
+
+.lpOptionalField {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+#lpPriceSettings {
+    input {
+        display: inline-block;
+        margin-left: 10px;
+        width: 50px;
+    }
+}
+
+#share .lpContent {
+    width: 330px;
+}
+
+#settings .lpContent {
+    width: 200px;
+}
+
+#share label {
+    font-weight: bold;
+}
+.lpPopover {
+    display: block;
+    position: relative;
+
+    .lpTarget {
+        cursor: default;
+        display: inline-block;
+        margin-bottom: -10px;
+        padding-bottom: 10px;
+        position: relative;
+    }
+
+    .lpContent {
+        background: #fff;
+        box-shadow: 0 0 6px rgba(0, 0, 0, 0.25);
+        color: $content;
+        left: 50%;
+        margin-top: 15px;
+        min-width: 100%;
+        opacity: 0;
+        padding: 12px;
+        pointer-events: none;
+        position: absolute;
+        top: 100%;
+        transform: translateX(-50%);
+        transition: all 0.15s;
+        white-space: nowrap;
+        z-index: $dialog;
+
+        &::before {
+            background-color: #fff;
+            box-shadow: 0 0 6px rgba(0, 0, 0, 0.25);
+            content: "";
+            display: block;
+            height: 20px;
+            left: 50%;
+            margin-left: -10px;
+            position: absolute;
+            top: -10px;
+            transform: rotate(45deg);
+            width: 20px;
+            z-index: $dialog - 1;
+        }
+
+        &::after {
+            background: #fff;
+            content: "";
+            display: block;
+            height: 15px;
+            left: 0;
+            position: absolute;
+            top: 0;
+            width: 100%;
+            z-index: $dialog + 1;
+        }
+
+        & > *:first-child {
+            margin-top: 0;
+        }
+
+        & > *:last-child {
+            margin-bottom: 0;
+        }
+
+        h3 {
+            margin-bottom: 0;
+        }
+
+        ul, a {
+            line-height: 25px;
+        }
+
+        hr {
+            border-color: $border1;
+            margin: 7px -0;
+            padding: 0;
+        }
+    }
+
+    &.lpPopoverShown {
+        .lpTarget {
+            z-index: $aboveDialog;
+        }
+
+        .lpContent {
+            margin-top: 10px;
+            opacity: 1;
+            pointer-events: all;
+        }
+    }
+}
+
+</style>
 <template>
     <span class="headerItem hasPopover">
-        <PopoverHover id="headerPopover">
-            <span slot="target">Signed in as <span class="username">{{ username }}</span> <i class="lpSprite lpExpand" /></span>
-            <div slot="content">
+        <Popper id="headerPopover">
+            <span><span class="username">{{ username }}</span> <i class="lpSprite lpExpand" /></span>
+            <template #content>
+              <div class="lpContent">
                 <a class="lpHref accountSettings" @click="showAccount">Account Settings</a><br>
                 <a class="lpHref" @click="showHelp">Help</a><br>
                 <a class="lpHref signout" @click="signout">Sign Out</a>
-            </div>
-        </PopoverHover>
+              </div>
+           </template>
+        </Popper>
     </span>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
-import PopoverHover from './popover-hover.vue';
+import Popper from "vue3-popper";
 
 export default defineComponent({
   name: 'AccountDropdown',
 
   components: {
-      PopoverHover,
+      Popper,
   },
 
   computed: {
--- client/components/share.vue
+++ client/components/share.vue
@@ -1,38 +1,165 @@
 <style lang="scss">
+@import "../css/_globals";
+#csvUrl {
+    display: block;
+    margin-top: 15px;
+}
+
+#lpOptionalFields {
+    margin: 0;
+    padding: 0;
+}
+
+.lpOptionalField {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+#lpPriceSettings {
+    input {
+        display: inline-block;
+        margin-left: 10px;
+        width: 50px;
+    }
+}
+
+#share .lpContent {
+    width: 330px;
+}
+
+#settings .lpContent {
+    width: 200px;
+}
+
 #share label {
     font-weight: bold;
 }
+.lpPopover {
+    display: block;
+    position: relative;
+
+    .lpTarget {
+        cursor: default;
+        display: inline-block;
+        margin-bottom: -10px;
+        padding-bottom: 10px;
+        position: relative;
+    }
+
+    .lpContent {
+        background: #fff;
+        box-shadow: 0 0 6px rgba(0, 0, 0, 0.25);
+        color: $content;
+        left: 50%;
+        margin-top: 15px;
+        min-width: 100%;
+        opacity: 0;
+        padding: 12px;
+        pointer-events: none;
+        position: absolute;
+        top: 100%;
+        transform: translateX(-50%);
+        transition: all 0.15s;
+        white-space: nowrap;
+        z-index: $dialog;
+
+        &::before {
+            background-color: #fff;
+            box-shadow: 0 0 6px rgba(0, 0, 0, 0.25);
+            content: "";
+            display: block;
+            height: 20px;
+            left: 50%;
+            margin-left: -10px;
+            position: absolute;
+            top: -10px;
+            transform: rotate(45deg);
+            width: 20px;
+            z-index: $dialog - 1;
+        }
+
+        &::after {
+            background: #fff;
+            content: "";
+            display: block;
+            height: 15px;
+            left: 0;
+            position: absolute;
+            top: 0;
+            width: 100%;
+            z-index: $dialog + 1;
+        }
+
+        & > *:first-child {
+            margin-top: 0;
+        }
+
+        & > *:last-child {
+            margin-bottom: 0;
+        }
+
+        h3 {
+            margin-bottom: 0;
+        }
+
+        ul, a {
+            line-height: 25px;
+        }
+
+        hr {
+            border-color: $border1;
+            margin: 7px -0;
+            padding: 0;
+        }
+    }
+
+    &.lpPopoverShown {
+        .lpTarget {
+            z-index: $aboveDialog;
+        }
+
+        .lpContent {
+            margin-top: 10px;
+            opacity: 1;
+            pointer-events: all;
+        }
+    }
+}
+
 </style>
 
 <template>
-    <span v-if="isSignedIn" class="headerItem hasPopover">
-        <PopoverHover id="share" @shown="focusShare">
-            <span slot="target"><i class="lpSprite lpLink" /> Share</span>
-            <div slot="content" class="lpFields">
-                <div class="lpField">
-                    <label for="shareUrl">Share your list</label>
-                    <input id="shareUrl" v-select-on-bus="'show-share-box'"  v-select-on-focus type="text" :value="shareUrl">
-                </div>
-                <div class="lpField">
-                    <label for="embedUrl">Embed your list</label>
-                    <textarea id="embedUrl" v-select-on-focus>&lt;script src="{{ this.baseUrl }}/e/{{ this.externalId }}"&gt;&lt;/script&gt;&lt;div id="{{ this.externalId }}"&gt;&lt;/div&gt;</textarea>
-                </div>
-                <a id="csvUrl" :href="csvUrl" target="_blank" class="lpHref"><i class="lpSprite lpSpriteDownload" />Export to CSV</a>
+  <span id="share" v-if="isSignedIn" class="headerItem hasPopover">
+      <Popper>
+        <span class="lpTarget"><i class="lpSprite lpLink" /> Share</span>
+        <template #content>
+          <div class="lpContent">
+            <div class="lpField">
+              <label for="shareUrl">Share your list</label>
+              <input id="shareUrl" v-select-on-bus="'show-share-box'"  v-select-on-focus type="text" :value="shareUrl">
             </div>
-        </PopoverHover>
-    </span>
+            <div class="lpField">
+              <label for="embedUrl">Embed your list</label>
+              <textarea id="embedUrl" v-select-on-focus>&lt;script src="{{ this.baseUrl }}/e/{{ this.externalId }}"&gt;&lt;/script&gt;&lt;div id="{{ this.externalId }}"&gt;&lt;/div&gt;</textarea>
+            </div>
+            <a id="csvUrl" :href="csvUrl" target="_blank" class="lpHref"><i class="lpSprite lpSpriteDownload" />Export to CSV</a>
+          </div>
+        </template>
+      </Popper>
+  </span>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
-import PopoverHover from './popover-hover.vue';
+import Popper from "vue3-popper";
 
 export default defineComponent({
   name: 'Share',
 
   components: {
-      PopoverHover,
+      Popper,
   },
 
   computed: {
@@ -84,4 +211,27 @@ export default defineComponent({
       },
   },
 });
+/*
+
+<template>
+  <span id="share" v-if="isSignedIn" class="headerItem">
+      <Popper>
+        <span>Share</span>
+        <template #content>
+          <div>
+            <div class="lpField">
+              <label for="shareUrl">Share your list</label>
+              <input id="shareUrl" v-select-on-bus="'show-share-box'"  v-select-on-focus type="text" :value="shareUrl">
+            </div>
+            <div class="lpField">
+              <label for="embedUrl">Embed your list</label>
+              <textarea id="embedUrl" v-select-on-focus>&lt;script src="{{ this.baseUrl }}/e/{{ this.externalId }}"&gt;&lt;/script&gt;&lt;div id="{{ this.externalId }}"&gt;&lt;/div&gt;</textarea>
+            </div>
+            <a id="csvUrl" :href="csvUrl" target="_blank" class="lpHref"><i class="lpSprite lpSpriteDownload" />Export to CSV</a>
+          </div>
+        </template>
+      </Popper>
+  </span>
+</template>
+*/
 </script>
