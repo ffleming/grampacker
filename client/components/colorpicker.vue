2acbb9524713899fdaff00522f44df495e537008
Forrest Fleming <ffleming@gmail.com>
2024-05-21 22:53:48 -0700

    css works, placeholder styling

--- client/components/colorpicker.vue
+++ client/components/colorpicker.vue
@@ -2,24 +2,26 @@
 </style>
 
 <template>
-    <Popover id="lpPickerContainer" :shown="shown" @hide="shown = false">
-        <span slot="target" class="lpLegend" :style="{'background-color': color}" @click="shown = true" />
-        <VueColorPicker slot="content" :width="150" :height="150" :disabled="false" :start-color="color" @color-change="onColorChange" />
-    </Popover>
+    <Popper id="lpPickerContainer" :shown="shown" @hide="shown = false">
+        <span class="lpLegend" :style="{'background-color': color}" @click="shown = true" />
+        <template #content>
+          <VueColorPicker :width="150" :height="150" :disabled="false" :start-color="color" @color-change="onColorChange" />
+        </template>
+    </Popper>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
 
+import { defineComponent } from 'vue';
+import Popper from "vue3-popper";
 import VueColorPicker from 'vue-color-picker-wheel';
-import Popover from './popover.vue';
 
 export default defineComponent({
   name: 'ColorPicker',
 
   components: {
       VueColorPicker,
-      Popover,
+      Popper,
   },
 
   props: [
