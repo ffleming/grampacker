8b5ab2fafec0113129152a921a102f1866adb933
Forrest Fleming <7241368+ffleming@users.noreply.github.com>
2026-04-06 20:28:33 -0700

    add end tag

--- client/components/help.vue
+++ client/components/help.vue
@@ -17,7 +17,7 @@
         </ol>
         <hr>
         <strong>Quantity and worn values</strong>
-        <p>If you have multiple quantity of an item and mark that item as worn, all of them will count towards your worn weight. This is because the app is geared towards ultralight hikers and backpackers, who do not generally have multiple of the exact same items that are split across worn and carried types. This means you can list your shoes/trekking poles with weights as individual weights and quantity of two, but if you bring three identical pairs of socks, you should separate them into worn and carried items.
+        <p>If you have multiple quantity of an item and mark that item as worn, all of them will count towards your worn weight. This is because the app is geared towards ultralight hikers and backpackers, who do not generally have multiple of the exact same items that are split across worn and carried types. This means you can list your shoes/trekking poles with weights as individual weights and quantity of two, but if you bring three identical pairs of socks, you should separate them into worn and carried items.</p>
         <hr>
         <strong>Items in multiple lists</strong>
         <p>If you copy your list or drag an item from the gear library into a second list, those items are now <strong>linked</strong>. This means that changes to an item in one place will update that list everywhere. If you want to copy your list without links, for now you can export to CSV and re-import the list.</p>
